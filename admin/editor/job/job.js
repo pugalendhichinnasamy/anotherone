40217ebd8b86f9990db476e4beac6be74e82b5dc
pugalendhichinnasamy <pugalendhi1995@gmail.com>
2025-03-21 15:41:52 +0530

    Update job.js

--- admin/editor/job/job.js
+++ admin/editor/job/job.js
@@ -1,1 +1,8 @@
+function addJob() {
+  const jobTitle = document.getElementById('jobTitle').value;
+  const jobList = document.getElementById('jobList');
+  const jobItem = document.createElement('p');
+  jobItem.textContent = `Job: ${jobTitle}`;
+  jobList.appendChild(jobItem);
+}
 
